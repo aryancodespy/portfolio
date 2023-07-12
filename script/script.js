0c85bfb3b13a875765fb52ab8f69f8a1e8f54655
Aryan <125204827+aryancodespy@users.noreply.github.com>
2023-07-12 19:48:20 +0600

    Modified scroll reveal

--- script/script.js
+++ script/script.js
@@ -50,9 +50,9 @@ ScrollReveal({
 });
 
 ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
-ScrollReveal().reveal('.home-img, .services-container, .portfolio-content, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-content, .blog-content, .contact form', { origin: 'bottom' });
 ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
-ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+ScrollReveal().reveal('.home-content p, .about-content, .blog .btn', { origin: 'right' });
 
 /*-------------------------------------- Typed Js --------------------------------------------*/
 
